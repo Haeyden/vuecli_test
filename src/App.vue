acd3aaef84362c49c5007328d37118a59848e89d
lemon <2467696975@qq.com>
2019-06-19 23:47:29 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <!-- 子组件 -->
+      <Add :addCommen='addCommen'/>
+      <List :comments='comments'/>
+    </div>
+  </div>
+</template>
+<script>
+// 1.引入
+import Add from "./comonents/Add.vue";
+import List from "./comonents/List.vue";
+export default {
+  //2.映射成标签
+  components: {
+    Add,
+    List
+  },
+  data(){
+      return{
+          comments:[
+              {name:'BOB',content:'Vue还不错'},
+              {name:'XOX',content:'Vue很强'},
+              {name:'LIS',content:'我比较渣'}
+          ]
+      }
+  },
+  methods:{
+      //添加评论
+      addCommen(comment){
+          this.comments.unshift(comment)
+      }
+  }
+};
+</script>
+<style>
+</style>
---
+++ src/comonents/Add.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="col-md-4">
+      <form class="form-horizontal">
+        <div class="form-group">
+          <label>用户名</label>
+          <input type="text" class="form-control" placeholder="用户名" v-model="name">
+        </div>
+        <div class="form-group">
+          <label>评论内容</label>
+          <textarea class="form-control" rows="6" placeholder="评论内容" v-model="content"></textarea>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-2 col-sm-10">
+            <button type="button" class="btn btn-default pull-right" @click="add">提交</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    //从父组件接收到的函数可以通过以下两种方式接收
+    // props:['addCommen'],
+    props:{
+        addCommen:{
+            type:Function,
+            required:true
+        }
+    },
+    data(){
+        return{
+            name:'',
+            content:''
+        }
+    },
+    methods:{
+        add(){
+            //收集数据 v-model
+            //1.检查数据合法性
+            const name=this.name.trim()
+            const content=this.content.trim()
+            if(!name || !content){
+                alert('姓名或者内容不能为空')
+            }
+            //2.根据输入的数据封装为comment对象
+            const comment={
+                name,
+                content
+            }
+            //3.添加到comments中
+            this.addCommen(comment)
+            //4.清除输入
+            this.name=''
+            this.content=''
+        }
+    }
+};
+</script>
+<style>
+</style>
